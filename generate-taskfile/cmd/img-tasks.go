58953e33ac14320ff0d74db79e8638176d2a42c1
Mark Ormesher <me@markormesher.co.uk>
2024-12-15 08:39:58 +0000

    fix: remove .version file check in image generation, it's never used

--- generate-taskfile/cmd/img-tasks.go
+++ generate-taskfile/cmd/img-tasks.go
@@ -25,9 +25,7 @@ fi
 
 img_name=$( (grep "LABEL image.name=" ` + p.ContainerFileName + ` || echo) | head -n 1 | cut -d '=' -f 2-)
 img_registry=$( (grep "LABEL image.registry=" ` + p.ContainerFileName + ` || echo) | head -n 1 | cut -d '=' -f 2-)
-if [[ -f .version ]]; then
-	version=":"$(cat .version)
-elif git describe --tags >/dev/null 2>&1; then
+if git describe --tags >/dev/null 2>&1; then
   version=":"$(git describe --tags)
 else
   version=""
