63d0f83cf39c3bd47359e3c112e8b33fe4e3ae70
Mark Ormesher <me@markormesher.co.uk>
2024-12-15 09:30:49 +0000

    feat: publish 'latest' and major version tags

--- generate-taskfile/cmd/img-tasks.go
+++ generate-taskfile/cmd/img-tasks.go
@@ -26,7 +26,10 @@ fi
 img_name=$( (grep "LABEL image.name=" ` + p.ContainerFileName + ` || echo) | head -n 1 | cut -d '=' -f 2-)
 img_registry=$( (grep "LABEL image.registry=" ` + p.ContainerFileName + ` || echo) | head -n 1 | cut -d '=' -f 2-)
 if git describe --tags >/dev/null 2>&1; then
-  version=":"$(git describe --tags)
+  version=$(git describe --tags)
+  major_version=$(echo "${version}" | cut -d '.' -f 1)
+  latest_version_overall=$(git tag -l | sort --reverse --version-sort | head -n 1)
+  latest_version_within_major=$(git tag -l | grep "^${major_version}" | sort --reverse --version-sort | head -n 1)
 else
   version=""
 fi
@@ -50,12 +53,10 @@ $builder build -f ` + p.ContainerFileName + ` .
 img=$($builder build -q -f ` + p.ContainerFileName + ` .)
 
 if [[ ! -z "$img_name" ]]; then
-  $builder tag "$img" "localhost/${img_name}${version}"
-  echo "Tagged localhost/${img_name}${version}"
+  $builder tag "$img" "localhost/${img_name}${version:+:$version}"
 
   if [[ ! -z "$img_registry" ]]; then
-    $builder tag "$img" "${img_registry}/${img_name}${version}"
-    echo "Tagged ${img_registry}/${img_name}${version}"
+    $builder tag "$img" "${img_registry}/${img_name}${version:+:$version}"
   fi
 else
   echo "Warning: no image name label; this image has not been tagged" >&2
@@ -93,7 +94,17 @@ elif [[ "$version" == "" ]]; then
   echo "Cannot determine image version - image will not be pushed" >&2
   exit 1
 else
-  $builder push "${img_registry}/${img_name}${version}"
+  $builder push "${img_registry}/${img_name}:${version}"
+
+  if [[ "${version}" == "${latest_version_within_major}" ]]; then
+    $builder tag "${img_registry}/${img_name}:${version}" "${img_registry}/${img_name}:${major_version}"
+    $builder push "${img_registry}/${img_name}:${major_version}"
+  fi
+
+  if [[ "${version}" == "${latest_version_overall}" ]]; then
+    $builder tag "${img_registry}/${img_name}:${version}" "${img_registry}/${img_name}:latest"
+    $builder push "${img_registry}/${img_name}:latest"
+  fi
 fi
 `},
 		},
